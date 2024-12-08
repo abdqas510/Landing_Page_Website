04a9c930cc18f30565bf2191ef6a4f3eca61143d
abdqas510 <abdqasrawi510@gmail.com>
2024-12-08 12:06:33 +0200

    added  a class to active section in the navigation bar depending on the page scroll

--- js/app.js
+++ js/app.js
@@ -41,13 +41,23 @@ const AddActiveClass = (section)=>{
     // set a background color for your active class
     section.setAttribute('style','background: linear-gradient(0deg, rgba(0, 0, 0, 0.7) 0%, rgba(0, 0, 0, 0.2) 100%);');  
 
+        navBarSectionsAnchor.forEach(anchor=>{
+
+            if(anchor.textContent===section.dataset.nav){
+
+                anchor.classList.add('active__section');
+            }
+            else{
+                anchor.classList.remove('active__section');
+            }
+        })
 }
 
 // removing the your-active-class
 const removeActiveClass = (section) => {
     section.classList.remove('your-active-class');
     // set the default color by CSS file on your active calss
-    section.setAttribute('style','background: linear-gradient(0deg, rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0) 100%);');    
+    section.setAttribute('style','background: linear-gradient(0deg, rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0) 100%);');           
 }
 /**
  * End Helper Functions
@@ -69,7 +79,6 @@ allSections.forEach(section=>{
 })
 
 navBarList.setAttribute('style','background-color:#F2F0EF');
-
 
 // go to near top of the section 
 const topOfSection = (section) =>{
@@ -107,8 +116,6 @@ navBarSectionsAnchor = document.body.querySelectorAll('#navbar__list a');
  * 
 */
 
-
-
 // Build menu 
 navBarSectionsAnchor.forEach(anchor =>{
     anchor.addEventListener('click',(evt)=>{
@@ -128,16 +135,4 @@ navBarSectionsAnchor.forEach(anchor =>{
         
     })
 })
-
-// Highlight the active section in the navigation bar
-navBarList.addEventListener('click',(event)=>{
-        // select all the sections in the nav bar
-        const navBarSections = navBarList.querySelectorAll('.menu__link');
-        // loop over them and set them to default [i coded that because i don't want the sections to hold there props after clicking on another section]
-        navBarSections.forEach(navBarSection=>navBarSection.setAttribute('style',"background-color:'';color:''"));
-        // set the clicked section props only 
-        event.target.setAttribute('style','background-color:#333;color:white');
-
-})   
-
 
